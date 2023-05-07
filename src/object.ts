84eb45e9704d76d8ec30de5541560ac7b47f91a1
farukwebcode21 <farukahmadmk@yahoo.com>
2023-05-07 17:18:59 +0600

    obect complete

---
+++ src/object.ts
@@ -0,0 +1,12 @@
+const heroObj: {
+  name: string;
+  readonly age: number;
+  profession: string;
+  isMarried?: boolean; //optional parameter
+} = {
+  name: "Shakib Khan",
+  age: 35,
+  profession: "Actor",
+};
+
+console.log(heroObj);
