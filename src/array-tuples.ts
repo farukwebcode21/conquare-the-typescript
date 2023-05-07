83412c9a9ed61d6ef82266ae3bc9337d1d5b5dc2
farukwebcode21 <farukahmadmk@yahoo.com>
2023-05-07 16:43:16 +0600

    start conceptual class typescript

---
+++ src/array-tuples.ts
@@ -0,0 +1,15 @@
+// array
+
+const heros: string[] = ["Rahim", "Karim", "Sumon", "Siam"];
+const nums: number[] = [1, 2, 3, 4];
+const heroInfo: (string | number | boolean)[] = ["Khan", "Actor", 32, true];
+
+// tuple
+
+const heroTuple: [String, number, boolean, string] = [
+  "Sk-Rahim",
+  34,
+  true,
+  "Rakibul",
+];
+console.log(heroInfo);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Primitive Data Type
+
+/**
+ * string
+ * number
+ * boolean
+ * null
+ * undefined
+ * 
+ * **/
+
+const str:string = "Hello World Typescript"
+console.log(str);
+
